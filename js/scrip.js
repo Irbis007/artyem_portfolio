508ca842c07aa5ec40f09f8ff007b6fa2b527844
Konstantin Shushkodomov <103035762+Irbis007@users.noreply.github.com>
2024-05-16 11:40:32 +0300

    Update scrip.js

--- js/scrip.js
+++ js/scrip.js
@@ -63,9 +63,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
 			let scrollOffset = el.offsetTop;
 			if (windowCenter >= scrollOffset || active) {
 				el.classList.add('animation-class');
-			} else {
-				el.classList.remove('animation-class');
-			}
+			} 
 		});
     }
 
@@ -76,9 +74,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
 			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
 			if (windowCenter >= scrollOffset || active) {
 				el.classList.add('animation-class');
-			} else {
-				el.classList.remove('animation-class');
-			}
+			} 
 		});
 	};
     function showModalByScroll(){
